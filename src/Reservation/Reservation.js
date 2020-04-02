efde2497e70a0d00deb07c1b30354b2cd6349091
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-02 09:40:04 -0600

    Style Reservations

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Reservation.css';
 
 const Reservation = (props) => {
   const { name, date, time, number } = props;
   return (
-    <article>
-      <h2>{name}</h2>
+    <article className='resy'>
+      <h3>{name}</h3>
       <p>{date}</p>
       <p>{time}</p>
       <p>Number of Guests: {number}</p>
